a543145f6975d5833c3a15339f2c1d79ddb7411f
samuFerragut <samufuerza@gmail.com>
2022-09-29 19:35:08 +0200

    feat: create POST route

--- src/routes/images.js
+++ src/routes/images.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 const Image = require('../models/Image');
 
+// GET route
 router.get('/', async (req, res) => {
     const images = await Image.find();
     res.json(images);
+})
+
+// POST route
+router.post('/', async(req, res) => {
+    const image = new Image(req.body);
+    await image.save();
+    res.json({
+        status: 'Image saved correctly'
+    });
 })
 
 module.exports = router;
